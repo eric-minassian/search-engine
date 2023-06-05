3eb065bdf50e0a0b6d8c2777d5924740d47ae30d
Eric Minassian <eric@ericminassian.com>
2023-06-04 20:55:03 -0700

    bug fixes

--- helpers/search.py
+++ helpers/search.py
@@ -17,11 +17,20 @@ class Search:
         if not stemmed_tokens:
             return []
 
-        document_ids = set()
+        document_ids = dict()
         for token in stemmed_tokens:
-            document_ids = document_ids.union(self.indexer.get_postings(token))
+            # Get the postings for the token.
+            postings = self.indexer.get_postings(token)
 
-        document_ids = list(document_ids)
+            # If there are no postings for the token, skip it.
+            if not postings:
+                continue
+
+            # Add the postings to the document ids.
+            for posting in postings:
+                document_ids[posting[0]] = document_ids.get(posting[0], 0) + posting[1]
+            
+        document_ids = list(document_ids.items())
 
         # Sort the document ids by their tf-idf score.
         document_ids.sort(key=lambda x: x[1], reverse=True)
